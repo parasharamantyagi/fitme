5399976c0b34d8f5ac14afa26291dfa72cb68e9b
vijay sandhu <vijay.pro2020@gmail.com>
2021-10-22 15:17:23 +0530

    fix code

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -215,7 +215,7 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="logo-pro">
-                        <a href="{{url('admin/dashboard')}}"><img class="main-logo" src="{{url('admin/img/logo/logo.png')}}" alt="" /></a>
+                        <a href="{{url('admin/dashboard')}}"><img class="main-logo" src="{{url('admin/img/logo/logo-icone2.png')}}" alt="" /></a>
                     </div>
                 </div>
             </div>
